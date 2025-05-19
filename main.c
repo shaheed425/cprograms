06e84b7d46d87b6722b03c8475fcc5c5cb02d60d
Shaheed Rahman CV <shaheedrahman99@gmail.com>
2025-05-19 09:39:24 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,28 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int i,j,k,l,size=5;
-    for(i=0;i<=size;i++){
-        for(j=0;j<=size-i;j++){
-            printf(" *");
+    int i,j,k,size=4;
+    for(i=1;i<=size;i++){
+        for(j=1;j<=size-i;j++){
+            printf(" ");
         }
+        for(k=1;k<=i;k++){
+            printf(" %d",i);
+        } 
         printf("\n");
+
+        
     }
-    for(i=0;i<=size;i++){
-        for(j=0;j<=i;j++){
-            printf(" *");
-        }
-        printf("\n");
-    }
+    
 
     return 0;
 }
