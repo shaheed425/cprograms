6b2023f187efe92e227b6ccd073e322014a1a63a
Shaheed Rahman CV <shaheedrahman99@gmail.com>
2025-05-19 09:32:25 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -10,43 +10,19 @@ Welcome to GDB Online.
 
 int main()
 {
-    int i,j,k,l,m,size=4,s;
-    for(i=1;i<=size;i++){
-        for(j=1;j<=i;j++){
-           
-            printf("*");
+    int i,j,k,l,size=5;
+    for(i=0;i<=size;i++){
+        for(j=0;j<=size-i;j++){
+            printf(" *");
         }
-        for(s=1;s<=2*(size-i);s++){
-            printf(" ");
-        }
-       
-        for(j=1;j<=i;j++){
-            printf("*");
-        }
-         printf("\n");
-       
-        
-    }
-    
-     for(i = size; i >= 1; i--) {
-       
-        for(k = 1; k <= i; k++) {
-            printf("*");
-        }
-
-        
-        for(s = 1; s <= 2*(size - i); s++) {
-            printf(" ");
-        }
-
-       
-        for(k = 1; k <= i; k++) {
-            printf("*");
-        }
-
         printf("\n");
     }
-    
+    for(i=0;i<=size;i++){
+        for(j=0;j<=i;j++){
+            printf(" *");
+        }
+        printf("\n");
+    }
 
     return 0;
 }
